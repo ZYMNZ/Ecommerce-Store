c5eefb0e9eda85dae41f326fd3c88068cb0f9fe3
moonflaked <loganluo288@gmail.com>
2023-11-04 18:32:49 -0400

    Finished sign up page

--- Views/Login/login.php
+++ Views/Login/login.php
@@ -15,16 +15,21 @@
             <header class="header">
                 <label class="headerFont">Welcome</label>
             </header>
-            <section class="loginRegistrationSection marginAuto">
-                <label class="hintLabel displayBlock">Email:</label>
-                <input type="text" name="email" class="inputField width100Percent"> <br/>
-                <label class="hintLabel displayBlock">Password:</label>
-                <input type="password" name="password" class="inputField width100Percent"> <br/>
-            </section>
-            <section class="signButtons marginAuto">
-                <input type="submit" name="submit" value="Sign in" class="defaultButtonStyling cursorPointer width100Percent borderNone"> <br/>
-                <input type="button" name="signUp" value="Sign up" class="defaultButtonStyling cursorPointer width100Percent borderNone">
-            </section>
+            <form action="" method="POST">
+                <section class="loginRegistrationSection marginAuto">
+                    <label class="hintLabel displayBlock">Email:</label>
+                    <input type="text" name="email" class="inputField width100Percent"> <br/>
+                    <label class="hintLabel displayBlock">Password:</label>
+                    <input type="password" name="password" class="inputField width100Percent"> <br/>
+                </section>
+                <section class="signButtons marginAuto">
+                    <input type="submit" name="submit" value="Sign in" href="" class="defaultButtonStyling cursorPointer width100Percent borderNone"> <br/>
+                    <a href="/?controller=registration&action=registration">
+                        <input type="button" name="signUp" value="Sign up" class="defaultButtonStyling cursorPointer width100Percent borderNone">
+                    </a>
+                </section>
+            </form>
+
         </section>
         <?php
             include_once "Views/General/footer.php";
