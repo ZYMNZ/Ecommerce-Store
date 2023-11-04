791d97f6398eb38a48ed92c5ce449c4788c42d0b
moonflaked <loganluo288@gmail.com>
2023-11-04 19:10:45 -0400

    Put adding home button to nav bar in a function

--- Views/General/navbar.php
+++ Views/General/navbar.php
@@ -1,20 +1,22 @@
 <nav>
     <section id="navBar" class="width100Percent displayFlex">
         <section id="logo" class="height100Percent"></section>
-
         <?php
-            global $controllerPrefix;
-            global $action;
-            if($controllerPrefix == "login" && $action == "login") {
+            function addHomeButtonNavBar() {
+                global $controllerPrefix;
+                global $action;
+                if($controllerPrefix == "login" && $action == "login") {
         ?>
-            <section>
-                <input type="button" href="" value="Home" class="defaultButtonStyling borderNone navBarButton cursorPointer signButtons">
-            </section>
-        <?php
+                    <section>
+                        <input type="button" href="" value="Home" class="defaultButtonStyling borderNone navBarButton cursorPointer signButtons">
+                    </section>
+                    <?php
+                }
             }
+                    ?>
+        <?php
+            addHomeButtonNavBar();
         ?>
-
-
 
     </section>
 </nav>
