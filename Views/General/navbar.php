82a3e8e4da9385d3badddf72b7a1f1241c43168a
moonflaked <loganluo288@gmail.com>
2023-11-04 14:28:43 -0400

    Added home button to login page

--- Views/General/navbar.php
+++ Views/General/navbar.php
@@ -1,5 +1,8 @@
 <nav>
-    <section id="navBar" class="width100Percent">
+    <section id="navBar" class="width100Percent displayFlex">
         <section id="logo" class="height100Percent"></section>
+        <section>
+            <input type="button" href="" value="Home" class="defaultButtonStyling borderNone navBarButton cursorPointer signButtons"></section>
+        </section>
     </section>
 </nav>
--- Views/Login/login.php
+++ Views/Login/login.php
@@ -1,8 +1,9 @@
 <html>
     <head>
         <title>Login</title>
-        <link rel="stylesheet" type="text/css" href="Views/styles/navbar.css">
+        <!--Keep general styles css file at the top to let sub css files override the general one-->
         <link rel="stylesheet" type="text/css" href="Views/styles/generalstyles.css">
+        <link rel="stylesheet" type="text/css" href="Views/styles/navbar.css">
         <link rel="stylesheet" type="text/css" href="Views/styles/login.css">
         <link rel="stylesheet" type="text/css" href="Views/styles/footer.css">
     </head>
@@ -15,14 +16,14 @@
                 <label class="headerFont">Welcome</label>
             </header>
             <section id="loginSection" class=" marginAuto">
-                <label class="hintLabel">Email:</label>
-                <input type="text" name="email" class="inputField"> <br/>
-                <label class="hintLabel">Password:</label>
-                <input type="password" name="password" class="inputField"> <br/>
+                <label class="hintLabel displayBlock">Email:</label>
+                <input type="text" name="email" class="inputField width100Percent"> <br/>
+                <label class="hintLabel displayBlock">Password:</label>
+                <input type="password" name="password" class="inputField width100Percent"> <br/>
             </section>
             <section class="signButtons marginAuto">
-                <input type="submit" name="submit" value="Sign in" class="defaultButtonStyling cursorPointer width100Percent"> <br/>
-                <input type="button" name="signUp" value="Sign up" class="defaultButtonStyling cursorPointer width100Percent">
+                <input type="submit" name="submit" value="Sign in" class="defaultButtonStyling cursorPointer width100Percent borderNone"> <br/>
+                <input type="button" name="signUp" value="Sign up" class="defaultButtonStyling cursorPointer width100Percent borderNone">
             </section>
         </section>
         <footer>
