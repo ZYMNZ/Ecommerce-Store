25de96f8240c15859406649e00a89a2ba583b5ab
Yaman Alhamamy <yamanmh2002@gmail.com>
2023-11-28 16:06:53 -0500

    added deleteUser function

--- Models/User.php
+++ Models/User.php
@@ -33,6 +33,76 @@ class User {
         );
     }
 
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getPhoneNumber(): string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): void
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
 
     private function initializeProperties(
         $pUserId,
@@ -168,74 +238,28 @@ class User {
         return $isSuccessful;
     }
 
-    public function getUserId(): int
+    public static function deleteUser ($pUserId) : ?array
     {
-        return $this->userId;
+        $conn = openDatabaseConnection();
+        $sqlQuery = "DELETE FROM `user` WHERE user_id = ?";
+        $prepareStmt = $conn->prepare($sqlQuery);
+        $prepareStmt->bind_param("i",$pUserId);
+        $isDeleted = $prepareStmt->execute();
+
+        if ($isDeleted){
+            return [
+                "isDeleted" => $isDeleted
+            ];
+        }
+        return null;
     }
 
-    public function setUserId(int $userId): void
-    {
-        $this->userId = $userId;
+    public static function deleteUserFromUserRole($pUserId){
+        var_dump($pUserId);
+        $result = self::deleteUser($pUserId);
+        var_dump($result);
+//        if ($result)
     }
 
-    public function getFirstName(): string
-    {
-        return $this->firstName;
-    }
-
-    public function setFirstName(string $firstName): void
-    {
-        $this->firstName = $firstName;
-    }
-
-    public function getLastName(): string
-    {
-        return $this->lastName;
-    }
-
-    public function setLastName(string $lastName): void
-    {
-        $this->lastName = $lastName;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): void
-    {
-        $this->email = $email;
-    }
-
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): void
-    {
-        $this->password = $password;
-    }
-
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): void
-    {
-        $this->description = $description;
-    }
-
-    public function getPhoneNumber(): string
-    {
-        return $this->phoneNumber;
-    }
-
-    public function setPhoneNumber(string $phoneNumber): void
-    {
-        $this->phoneNumber = $phoneNumber;
-    }
 
 }
--- Views/User/deleteBuyer.php
+++ Views/User/deleteBuyer.php
@@ -1,1 +1,6 @@
 <?php
+include_once "Views/General/session.php";
+notLoggedIn();
+notUser();
+//notAdmin();
+User::deleteUserFromUserRole($_GET['id']);
