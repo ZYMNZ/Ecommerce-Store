84f4a4e71bcbeea1576e47d27a8293e725c7e7dc
Yaman Alhamamy <yamanmh2002@gmail.com>
2023-11-28 21:45:12 -0500

    cleaning code

--- Views/User/viewBuyers.php
+++ Views/User/viewBuyers.php
@@ -53,18 +53,21 @@ include_once "Views/General/navbar.php";
     ?>
 </table>
 
-<script>
-    // Use the PHP value directly in the JavaScript code
-    document.querySelector('button[data-toggle="modal"]').addEventListener('click', () => {
-        var currentId = this.closest('tr').querySelector('#userId').value;
-        console.log(currentId);
-    });
-
-</script>
+<!--<script>-->
+<!--    // Use the PHP value directly in the JavaScript code-->
+<!--    document.querySelector('button[data-toggle="modal"]').addEventListener('click', () => {-->
+<!--        var currentId = this.closest('tr').querySelector('#userId').value;-->
+<!--        console.log(currentId);-->
+<!--    });-->
+<!---->
+<!--</script>-->
 <?php
-include_once 'Views/General/modal.php';
-modal('deleteBuyer', $id);
-include_once "Views/General/footer.php";
-?>
+//include_once 'Views/General/modal.php';
+//modal('deleteBuyer', $id);
+//include_once "Views/General/footer.php";
+//?>
+
+    <?php include_once "Views/General/adminFooter.php"?>
+
 </body>
 </html>
--- Views/User/viewSellers.php
+++ Views/User/viewSellers.php
@@ -57,19 +57,21 @@
 
     </table>
 
-    <script>
-        // Use the PHP value directly in the JavaScript code
-        document.querySelector('button[data-toggle="modal"]').addEventListener('click', () => {
-            var currentId = this.closest('tr').querySelector('#userId').value;
-            console.log(currentId);
-        });
+<!--    <script>-->
+<!--        // Use the PHP value directly in the JavaScript code-->
+<!--        document.querySelector('button[data-toggle="modal"]').addEventListener('click', () => {-->
+<!--            var currentId = this.closest('tr').querySelector('#userId').value;-->
+<!--            console.log(currentId);-->
+<!--        });-->
+<!---->
+<!--    </script>-->
+<!--    --><?php
+//    include_once 'Views/General/modal.php';
+//    modal('deleteSeller', $id);
+//    include_once "Views/General/footer.php";
+//    ?>
 
-    </script>
-    <?php
-    include_once 'Views/General/modal.php';
-    modal('deleteSeller', $id);
-    include_once "Views/General/footer.php";
-    ?>
+    <?php include_once "Views/General/adminFooter.php"?>
 
     </body>
 </html>
