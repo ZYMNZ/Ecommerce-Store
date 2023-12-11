8548fe8a65d2d76b014a1e7c2385f7194059becc
Yaman Alhamamy <yamanmh2002@gmail.com>
2023-12-10 22:40:05 -0500

    fixing conflict

--- Controllers/CartController.php
+++ Controllers/CartController.php
@@ -3,28 +3,28 @@ include_once "Views/General/session.php";
 include_once 'Models/Product.php';
 include_once 'Models/Order.php';
 include_once 'Models/OrderProduct.php';
-    class CartController {
+    class CartController
+    {
         function route(): void
         {
             global $action;
-            if($action == "cart") {
+            if ($action == "cart") {
                 $displayCart = new Order();
                 $display = $displayCart->displayCart($_SESSION['user_id']);
                 $categories = Category::listCategories();
 //                var_dump($display);
                 if ($display != null) {
                     $this->render($action, ['display' => $display, 'categories' => $categories]);
-                    }
+                }
 //                if ($display !== null) {
 //                    $this->render($action, $display);
 //                }
 //                else{
-                    $this->render($action);
+                $this->render($action);
 //                }
             } else if ($action == "addToCart") {
                 $this->render($action);
-            }
-            else if ($action == "deleteCartProduct"){
+            } else if ($action == "deleteCartProduct") {
                 $this->render($action);
             }
         }
